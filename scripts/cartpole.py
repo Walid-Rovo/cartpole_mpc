fc8c0340cc73e22eb28bb858aa109960ef015f8e
Walid Ismail <walid.ismail@micropsi-industries.com>
2023-06-24 20:44:16 +0200

    First ASB Opti working ver

--- scripts/cartpole.py
+++ scripts/cartpole.py
@@ -1,21 +1,20 @@
-"""
-Reference: https://github.com/Farama-Foundation/Gymnasium/blob/main/gymnasium/envs/classic_control/cartpole.py
-"""
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from casadi import *
+import aerosandbox as asb
+import aerosandbox.numpy as np
 
+import pygame
 
-class PendulumOnCart:
-    # TODO: figure out why using theta=3.1 put the pole in the down position. It should be up
-    def __init__(self, initial_states=[0.0, 0.0, 0.1, 0.0], dt=0.02, render=False):
-        # physical parameters
+class CartPole:
+    def __init__(
+        self,
+        dt=0.02,
+        render=True,
+        ) -> None:
+        self.dt = dt
+        # Initial conditions
         self.g = -9.81
         self.mc = 2.4
         self.mp = 0.23
         self.mtotal = self.mc + self.mp
-        self.dt = dt
 
         # self.length = 0.5  # actually half the pole's length
         # self.polemass_length = self.masspole * self.length
@@ -24,117 +23,122 @@ class PendulumOnCart:
         self.force_mag = 1.0
         self.tau = 0.02  # seconds between state updates
 
-        # # states
-        self.x = 0
-        self.x_dot = 0
-        self.theta = 0
-        self.theta_dot = 0
-
-        # CasADi function instantiation
-        # x[0] = x, x[1] = x_dot, x[2] = theta, x[3] = theta_dot
-        self.nx = 4
-        self.nu = 1
-        self.x_sym = SX.sym("x", self.nx, 1)
-        self.u_sym = SX.sym("u", self.nu, 1)
-
-        # self.xdot_sym = vertcat(
-        #     # x[0]_dot =
-        #     self.x_sym[1],
-        #     # self.x_sym[1]_dot =
-        #     (
-        #         self.u_sym
-        #         + self.mp * self.lp * sin(self.x_sym[2]) * self.x_sym[3] ** 2
-        #         - self.mp * self.g * cos(self.x_sym[2]) * sin(self.x_sym[2])
-        #     )
-        #     / (self.mc + self.mp - self.mp * cos(self.x_sym[2]) ** 2),
-        #     # self.x_sym[2]_dot =
-        #     self.x_sym[3],
-        #     # self.x_sym[3]_dot =
-        #     (
-        #         self.u_sym * cos(self.x_sym[2])
-        #         + (self.mc + self.mp) * self.g * sin(self.x_sym[2])
-        #         + self.mp * self.lp * cos(self.x_sym[2]) * sin(self.x_sym[2]) * self.x_sym[3] ** 2
-        #     )
-        #     / (self.mp * self.lp * cos(self.x_sym[2]) ** 2 - (self.mc + self.lp) * self.lp),
-        # )
-        self.xdot_sym = vertcat(
-            # x[0]_dot =
-            self.x_sym[1],
-            # self.x_sym[1]_dot =
-            (self.u_sym + self.mp * self.lp * sin(self.x_sym[2]) * self.x_sym[3] ** 2 - self.mp * self.g * cos(self.x_sym[2]) * sin(self.x_sym[2]))
-            / (self.mc + self.mp - self.mp * cos(self.x_sym[2]) ** 2),
-            # self.x_sym[2]_dot =
-            self.x_sym[3],
-            # self.x_sym[3]_dot =
-            (
-                self.u_sym * cos(self.x_sym[2])
-                + (self.mc + self.mp) * self.g * sin(self.x_sym[2])
-                + self.mp * self.lp * cos(self.x_sym[2]) * sin(self.x_sym[2]) * self.x_sym[3] ** 2
-            )
-            / (self.mp * self.lp * cos(self.x_sym[2]) ** 2 - (self.mc + self.lp) * self.lp),
-        )
-        self.system = Function("system", [self.x_sym, self.u_sym], [self.xdot_sym])
-        # CasADi integrator instantiation
-        self.x_sym0 = np.array(initial_states).reshape(self.nx, 1)
-        # The CasADi integrator needs a dictionary of the states ('x'), the system state-space eqself.ations as a
-        # CasADi symbolic expression ('ode'), input ('p').
-        self.ode = {"x": self.x_sym, "ode": self.xdot_sym, "p": self.u_sym}
-
-        # By default, the solver integrates from 0 to 1. We change the final time to dt.
-        self.opts = {"tf": self.dt}
-
-        # Create the solver object.
-        # https://casadi.sourceforge.net/api/html/db/d3d/classcasadi_1_1Integrator.html#:~:text=Constructor%20%26%20Destructor%20Documentation
-        #                      name, solv typ, function dict, options dict
-        self.ode_solver = integrator("F", "idas", self.ode, self.opts)
-
-        # render
-        self.world_width = 10  # 4.8
-        self.screen_width = 1600  # 600
-        self.screen_height = 480  # 400
+        self.render_bool = render
+        self.isopen = True
         self.screen = None
         self.clock = None
-        self.isopen = True
-        self.render_bool = render
-        self.render_fps = int(1 / self.dt)
-        self.player_action = 0.0
-        self.pact_step = 0.5
-        self.pact_max = 1.5
         self.quit_flag = False
 
-    def step(self, action: float):
-        action = np.array([[action]])
-        res_integrator = self.ode_solver(x0=self.x_sym0, p=action)
-        # integrator returns dict, one of the values is the final x
-        self.x_sym0 = res_integrator["xf"]
+    def ode(self, X, U):
+        X_next = np.array([
+            # dx =
+            [X[1]],
+            # ddx =
+            [(U + self.mp * self.lp * np.sin(X[2]) * X[3] ** 2
+              - self.mp * self.g * np.cos(X[2]) * np.sin(X[2]))
+             / (self.mc + self.mp - self.mp * np.cos(X[2]) ** 2)],
+            # dtheta =
+            [X[3]],
+            # ddtheta =
+            [(
+                U * np.cos(X[2])
+                + (self.mc + self.mp) * self.g * np.sin(X[2])
+                + self.mp * self.lp * np.cos(X[2]) * np.sin(X[2]) * X[3] ** 2
+            ) / (self.mp * self.lp * np.cos(X[2]) ** 2 - (self.mc + self.lp) * self.lp)],
+        ])
+        if len(X_next.shape) > 2:
+            X_next = np.squeeze(X_next, axis=2)
+        return X_next
 
-        self.x = self.x_sym0[0]
-        self.x_dot = self.x_sym0[1]
-        self.theta = self.x_sym0[2]
-        self.theta_dot = self.x_sym0[3]
+    def objective_fun(self, X, U, Q, R):
+        return X.T @ Q @ X + U.T @ R @ U
+
+    def integrate_Euler(self, X, U, dt):
+        X_p = X + dt * self.ode(X, U)
+        return X_p
+
+    def integrate_RK4(self, X, U, dt):
+        k1 = self.ode(X, U)
+        k2 = self.ode(X + k1 * dt / 2, U)
+        k3 = self.ode(X + k2 * dt / 2, U)
+        k4 = self.ode(X + k3 * dt, U)
+        return X + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+    def ocp(self, x_0, x_f, Horiz, x_lims, u_lims):
+        # opti = cas.Opti()
+
+        # X = opti.variable(4, 11)
+        # U = opti.variable(1, 10)
+
+        nx = 4
+        nu = 1
+
+        def ix(i): return range(i * nx, (i + 1) * nx)  # noqa: E704
+
+        opti = asb.Opti()
+
+        X = opti.variable(np.zeros(nx * (Horiz + 1)))
+        U = opti.variable(np.zeros(nu * Horiz))
+
+        # U_lb = opti.parameter()
+        # U_ub = opti.parameter()
+
+        # opti.subject_to(-3.0 >= U[0])
+
+        # Initial condition
+        opti.subject_to(X[ix(0)] == x_0)
+
+        # System dynamics constraints and cost function
+        J = 0
+        R = 0.0001
+        Q = np.diag(np.array([0.01, 0.01, 1.0, 0.01]))
+        for i in range(Horiz):
+            opti.subject_to(X[ix(i + 1)] == self.integrate_RK4(X[ix(i)], U[i], self.dt))
+            # opti.subject_to(x_lims[0] <= X[ix(i)] <= np.tile(x_lims[1], Horiz + 1))
+            # opti.subject_to(U_lb <= U <= U_ub)
+            opti.subject_to(-50.0 <= U[i])
+            opti.subject_to(U[i] <= 50.0)
+            J += self.objective_fun(X[ix(i + 1)], U[i], Q, R)
+
+        Qf = np.diag(np.array([0.01, 0.01, 0.01, 0.01]))
+        J += X[ix(Horiz)].T @ Qf @ X[ix(Horiz)]
+
+        # Terminal state constraint
+        # opti.subject_to(X[ix(Horiz)] == x_f)
+
+        # Path constraints
+        # opti.set_value(U_lb, u_lims[0])
+        # opti.set_value(U_ub, u_lims[1])
+        # opti.subject_to(np.tile(x_lims[0], Horiz + 1) <= X <= np.tile(x_lims[1], Horiz + 1))
+        # opti.subject_to(np.tile(u_lims[0], Horiz + 1) <= U <= np.tile(u_lims[1], Horiz + 1))
+
+        # Solver and solving
+        opti.minimize(J)
+        opti.solver('ipopt')
+        soln = opti.solve(verbose=False, max_iter=20)
+
+        return soln.value(X), soln.value(U)
+
+    def forward_step(self, X, U):
+        X = self.integrate_RK4(X, U, self.dt)
 
         if self.render_bool:
-            self.render()
+            self.render(X)
+        return X
 
-        state = np.array(
-            [self.x.full(), self.x_dot.full(), self.theta.full(), self.theta_dot.full()]
-        ).reshape(-1, 1)
+    # def reset(self):
+    #     # state/observations.
+    #     X = np.zeros_like(X)
 
-        return state
-
-    def reset(self):
-        # state/observations.
-        self.x, self.x_dot, self.theta, self.theta_dot = (
-            0,
-            0,
-            0,
-            0,
-        )
-
-    def set_states(self, given_state):
-        self.x, self.x_dot, self.theta, self.theta_dot = given_state
-
-    def render(self):
+    def render(self, X):
+        # render
+        world_width = 10  # 4.8
+        screen_width = 1600  # 600
+        screen_height = 480  # 400
+        render_fps = int(1 / self.dt)
+        # player_action = 0.0
+        # pact_step = 0.5
+        # pact_max = 1.5
         try:
             import pygame
             from pygame import gfxdraw
@@ -146,51 +150,45 @@ class PendulumOnCart:
         if self.screen is None:
             pygame.init()
             pygame.display.init()
-            self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+            self.screen = pygame.display.set_mode((screen_width, screen_height))
         if self.clock is None:
             self.clock = pygame.time.Clock()
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.player_action -= self.pact_step
-        elif keys[pygame.K_RIGHT]:
-            self.player_action += self.pact_step
-        else:
-            self.player_action = 0.0
-        if self.player_action > self.pact_max:
-            self.player_action = self.pact_max
-        elif self.player_action < -self.pact_max:
-            self.player_action = -self.pact_max
+        # if keys[pygame.K_LEFT]:
+        #     player_action -= pact_step
+        # elif keys[pygame.K_RIGHT]:
+        #     player_action += pact_step
+        # else:
+        #     player_action = 0.0
+        # if player_action > pact_max:
+        #     player_action = pact_max
+        # elif player_action < -pact_max:
+        #     player_action = -pact_max
 
         if keys[pygame.K_q]:
             self.quit_flag = True
+            self.close()
 
         world_width = 40
-        scale = self.screen_width / world_width
+        scale = screen_width / world_width
         polewidth = 7.0
         polelen = scale * (4 * self.lp)
         cartwidth = polelen * 2 *  0.4
         cartheight = polelen * 2 * 0.2
 
-        x = (
-            self.x,
-            self.x_dot,
-            self.theta,
-            self.theta_dot,
-        )
-
-        self.surf = pygame.Surface((self.screen_width, self.screen_height))
-        self.surf.fill((255, 255, 255))
+        surf = pygame.Surface((screen_width, screen_height))
+        surf.fill((255, 255, 255))
 
         l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
         axleoffset = cartheight / 4.0
-        cartx = x[0] * scale + self.screen_width / 2.0  # MIDDLE OF CART
+        cartx = X[0] * scale + screen_width / 2.0  # MIDDLE OF CART
         carty = 100  # TOP OF CART
         cart_coords = [(l, b), (l, t), (r, t), (r, b)]
         cart_coords = [(c[0] + cartx, c[1] + carty) for c in cart_coords]
-        gfxdraw.hline(self.surf, 0, self.screen_width, carty, (0, 0, 0))
-        gfxdraw.aapolygon(self.surf, cart_coords, (0, 0, 0))
-        gfxdraw.filled_polygon(self.surf, cart_coords, (0, 0, 0))
+        gfxdraw.hline(surf, 0, screen_width, carty, (0, 0, 0))
+        gfxdraw.aapolygon(surf, cart_coords, (0, 0, 0))
+        gfxdraw.filled_polygon(surf, cart_coords, (0, 0, 0))
 
         l, r, t, b = (
             -polewidth / 2,
@@ -201,22 +199,22 @@ class PendulumOnCart:
 
         pole_coords = []
         for coord in [(l, b), (l, t), (r, t), (r, b)]:
-            coord = pygame.math.Vector2(coord).rotate_rad(-x[2])
+            coord = pygame.math.Vector2(coord).rotate_rad(-X[2])
             coord = (coord[0] + cartx, coord[1] + carty + axleoffset)
             pole_coords.append(coord)
-        gfxdraw.aapolygon(self.surf, pole_coords, (202, 152, 101))
-        gfxdraw.filled_polygon(self.surf, pole_coords, (202, 152, 101))
+        gfxdraw.aapolygon(surf, pole_coords, (202, 152, 101))
+        gfxdraw.filled_polygon(surf, pole_coords, (202, 152, 101))
 
         try:
             gfxdraw.aacircle(
-                self.surf,
+                surf,
                 int(cartx),
                 int(carty + axleoffset),
                 int(polewidth / 2),
                 (129, 132, 203),
             )
             gfxdraw.filled_circle(
-                self.surf,
+                surf,
                 int(cartx),
                 int(carty + axleoffset),
                 int(polewidth / 2),
@@ -225,42 +223,45 @@ class PendulumOnCart:
         except OverflowError:
             print(cartx, carty)
 
-        self.surf = pygame.transform.flip(self.surf, False, True)
-        self.screen.blit(self.surf, (0, 0))
+        surf = pygame.transform.flip(surf, False, True)
+        self.screen.blit(surf, (0, 0))
 
         pygame.event.pump()
-        self.clock.tick(self.render_fps)
+        self.clock.tick(render_fps)
         pygame.display.flip()
 
     def close(self):
         if self.screen is not None:
             import pygame
 
-            pygame.display.quit()
             pygame.quit()
+            # pygame.display.quit()
             self.isopen = False
 
 
 if __name__ == "__main__":
+    # Initial conditions
+    x_0 = np.array([[0.0],
+                    [0.0],
+                    [np.deg2rad(5.0)],
+                    [0.0]])
 
-    pendulum = PendulumOnCart(render=True)
+    x_f = np.zeros_like(x_0)
 
-    # pendulum.masspole = 0.01
-    N_SIM_STEPS = 1000
-    N_FORCE_STEPS = 5
-    FORCE_MAGNITUDE = 0.1  # Newtons
-    N_FORCE_STEPS = 50
+    x_lims = (np.ones(4, dtype="float32") * -3.14,
+              np.ones(4, dtype="float32") * 3.14,)
+    u_lims = (-3.0, 3.0)
 
-    # random normal noise force
-    np.random.seed(42)
-    force_vec = np.zeros(N_SIM_STEPS)
-    force_vec[:N_FORCE_STEPS] = np.random.normal(loc=0, scale=FORCE_MAGNITUDE, size=N_FORCE_STEPS)
+    # Init CartPole with init. conditions
+    cartpole = CartPole(render=True)
 
-    pendulum.reset()
-    pendulum.pact_step = 1.5
-    pendulum.pact_max = 10.0
+    counter = 0
     try:
         while True:
-            pendulum.step(action=pendulum.player_action)
+            _, u = cartpole.ocp(x_0, x_f, 5, x_lims, u_lims)
+            x_0 = cartpole.forward_step(x_0, u[0])
+            print(u[0], x_0[2])
+
+            # x_0 = cartpole.forward_step(x_0, 4.5492890003726085)
     except KeyboardInterrupt:
-        pendulum.close()
+        raise SystemExit from KeyboardInterrupt
